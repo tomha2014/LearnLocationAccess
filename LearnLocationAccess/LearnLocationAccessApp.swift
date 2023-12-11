fcf2308375c55ed0ce5ef12711377bd638b6e01e
TomHa <tomhackbarth@gmail.com>
2023-12-11 18:50:58 -0500

    Initial Commit

---
+++ LearnLocationAccess/LearnLocationAccessApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLocationAccessApp.swift
+//  LearnLocationAccess
+//
+//  Created by tom hackbarth on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnLocationAccessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
